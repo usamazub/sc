a3fdef06ea609ce219ec4ef62946ae4cc919e199
Usama <usamazub@gmail.com>
2018-11-16 19:25:40 +0700

    edit primbon.py

--- main/primbon.py
+++ main/primbon.py
@@ -1,2 +1,16 @@
+import datetime
+
+# suhu (min, mean, max)
+# kelembapan, curah hujan
+# lama penyinaran
+# kecepatan angin
+# arah angin terbanyak
+# arah angin terbesar
+# arah angin saat kecepatan maksimum
+
 def get_result(duration):
+    day = datetime.datetime.now().strftime("%d")
+    month = datetime.datetime.now().strftime("%B")
+    year = datetime.datetime.now().strftime("%Y")
+
     return {}
